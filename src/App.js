24d195a4178b483db51f4747843ac9a1134c862a
raylee45 <raylee45@gmail.com>
2022-10-04 19:32:18 -0700

    adding component files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import the Game component
+import Game from './components/Game';
+
+function App() {
+    return (
+        <div>
+            <Game />
+        </div>
+    )
+}
+
+export default App;
